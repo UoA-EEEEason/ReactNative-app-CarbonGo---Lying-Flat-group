7b3c624fa290e675d19edefffe36e79a153b78da
xinyaMega <xinya@megatransact.com>
2023-12-07 14:58:03 +1300

    firebase demo provide a demo for createUser and logout

---
+++ src/hooks/firebase/index.js
@@ -0,0 +1,4 @@
+import {createUser} from './createUser';
+import {logout} from './logout'
+
+export {createUser, logout};
--- src/screens/authScreens/LoginScreen.jsx
+++ src/screens/authScreens/LoginScreen.jsx
@@ -4,23 +4,39 @@ import {
   StatusBar,
   ScrollView,
   Text,
+  Button,
 } from 'react-native';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { COLORS } from './../../constants/color/color';
 import TestComponent from './../../components/TestComponent';
+import auth from '@react-native-firebase/auth';
+import {createUser,logout} from './../../hooks/firebase';
 
 const LoginScreen = () => {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+  
   return (
-    <SafeAreaView>
-      <StatusBar backgroundColor={COLORS.blue} />
-      <ScrollView>
-        <View>
-          <Text>Login screen</Text>
-          <TestComponent />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+    <View>
+      {(!user) ? <Text>Login</Text>:<Text>Welcome {user.email}</Text> }
+      <Button title="create user" onPress={createUser} />
+      <Button title="logout" onPress={logout} />
+    </View>
+  )
 };
 
 export default LoginScreen;
